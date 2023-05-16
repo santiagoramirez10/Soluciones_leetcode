7fc6001e6b86c91718cc4003ed01e63cf71da879
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2023-05-16 15:41:23 -0500

    Challlenges

--- Python/Easy.py
+++ Python/Easy.py
@@ -22,3 +22,34 @@ class Solution(object):
         """
         x=str(x)
         return(x[::-1]==x)
+#28. Find the Index of the First Occurrence in a String
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            pos=haystack.index(needle)
+        else:
+            pos=-1
+        return(pos)
+#35. Search Insert Position
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        try:
+            pos=nums.index(target)
+        except:
+            pos=0
+            while pos<len(nums):
+                if target<nums[pos]:
+                    break
+                else:
+                    pos+=1
+        return(pos)
